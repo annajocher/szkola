f37166a1a11a66b8b50be3e404ff9e95831f92b0
annajocher <ajocher@gmail.com>
2019-06-18 17:53:31 +0200

    pierwszy komit

---
+++ src/sample/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final double euro = 4.26743148;
+        final double dolar = 3.797502;
+        Scanner scanner = new Scanner(System.in);
+        double zloty = scanner.nextDouble();
+        System.out.println("przeliczone kursy");
+        scanner.close();
+    }
+}
